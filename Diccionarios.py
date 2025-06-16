d00dfe474eb1e345a9538e56c59d782012e8de49
cristiangodoyangel <creacciones@gmail.com>
2025-06-16 13:31:10 -0400

    ejercicios de diccionarios

---
+++ Diccionarios.py
@@ -0,0 +1,23 @@
+# personas = {}  # Diccionario vacío
+
+# cantidad = int(input("¿Cuántas personas vas a ingresar?: "))
+
+#for i in range(cantidad):
+#    nombre = input("Ingrese el nombre de la persona: ")
+#    edad = int(input(f"Ingrese la edad de {nombre}: "))
+#    personas[nombre] = edad  # Agrega al diccionario
+
+#print("\nDiccionario final:")
+#print(personas)
+#print("\nEl programa ha terminado")
+
+
+#Ejercicio 1: Diccionario de países y capitales
+#Pide al usuario que ingrese el nombre de un país y su capital. Guarda esos datos en un diccionario. Hazlo para 3 países.
+#paises={}
+#for i in range (3):
+#    pais=input("Ingrese nombre del país: ")
+#    capital=input(f"Ingrese Capitál de {pais}: ")
+#    paises [pais] = capital
+
+#print(paises)
